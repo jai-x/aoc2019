c058d6a4ba9234ad281c6ba7ab6d283801577466
Jai Sharma <jai.p.sharma1@gmail.com>
2019-12-04 17:01:21 +0000

    Add solution picker

--- aoc2019.c
+++ aoc2019.c
@@ -4,12 +4,30 @@
 #include "aoc2019.h"
 
 int
-main(void)
+main(int argc, char** argv)
 {
-	day1();
-	day2();
-	printf("TODO: Make Day 3 faster...\n");
-	day3();
-	day4();
+	if (argc < 2) {
+		printf("Usage: ./aoc2019 {day_num}\n");
+		return EXIT_FAILURE;
+	}
+
+	char day = argv[1][0];
+	switch (day) {
+	case '1':
+		day1();
+		break;
+	case '2':
+		day2();
+		break;
+	case '3':
+		printf("TODO: Make Day 3 faster...\n");
+		day3();
+		break;
+	case '4':
+		day4();
+		break;
+	default:
+		printf("No solution for Day %c\n", day);
+	}
 	return EXIT_SUCCESS;
 }
