e50b1be617f817e35fa6df540119a68f90bf4e54
Jai Sharma <jai.p.sharma1@gmail.com>
2019-12-09 12:09:58 +0000

    Improve Day 2

--- day2.c
+++ day2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "aoc2019.h"
 
@@ -28,55 +29,75 @@ intcode(int* memory)
 	}
 }
 
-void
-day2(void)
+static char*
+dump_file(FILE* file)
 {
-	// Open file
-	FILE* input_file = fopen(filename, "r");
-	if (!input_file) {
-		printf("Error opening file: %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-
 	// Get num bytes
-	fseek(input_file, 0, SEEK_END);
-	size_t input_num_bytes = (size_t)ftell(input_file);
-	fseek(input_file, 0, SEEK_SET);
+	fseek(file, 0, SEEK_END);
+	size_t file_bytes = (size_t)ftell(file);
+	fseek(file, 0, SEEK_SET);
 
-	// Allocate to buffer and close file
-	char* input_buffer = malloc(sizeof(char) * input_num_bytes);
-	fread(input_buffer, sizeof(char), input_num_bytes, input_file);
-	fclose(input_file);
+	// Allocate buffer and dump file
+	char* file_buffer = malloc(sizeof(char) * file_bytes);
+	fread(file_buffer, sizeof(char), file_bytes, file);
 
-	// Get memory entries
+	return file_buffer;
+}
+
+static size_t
+parse_memory_size(char* line)
+{
 	size_t num_commas = 0;
-	for (size_t i = 0; i < input_num_bytes; i++) {
-		if (input_buffer[i] == ',') {
+	for (size_t i = 0; i < strlen(line); i++) {
+		if (line[i] == ',') {
 			num_commas++;
 		}
 	}
-	size_t memory_size = num_commas + 1;
 
-	// Allocate program memory
+	return num_commas + 1;
+}
+
+static int*
+parse_memory(char* line, size_t memory_size)
+{
 	int* memory = malloc(sizeof(int) * memory_size);
 
-	// Read into array and free buffer
-	char* buffer_remainder = input_buffer;
-	char* token = NULL;
-	int read_pos = 0;
-	while ((token = strsep(&buffer_remainder, ","))) {
-		memory[read_pos++] = atoi(token);
+	char* line_remainder = line;
+	char *token = NULL;
+	size_t pos = 0;
+
+	while ((token = strsep(&line_remainder, ","))) {
+		memory[pos++] = atoi(token);
 	}
-	free(input_buffer);
+
+	return memory;
+}
+
+void
+day2(void)
+{
+	// Open file and dump to buffer
+	FILE* file = fopen(filename, "r");
+	assert(file);
+	char* file_buffer = dump_file(file);
+	fclose(file);
+
+	// Parse memory
+	size_t memory_size = parse_memory_size(file_buffer);
+	int* memory = parse_memory(file_buffer, memory_size);
+	free(file_buffer);
 
 	// Allocate for copy of memory
 	int* memory_copy = malloc(sizeof(int) * memory_size);
 
 	// Part 1
 	memcpy(memory_copy, memory, sizeof(int) * memory_size);
+
 	memory_copy[1] = 12;
 	memory_copy[2] = 2;
+
 	intcode(memory_copy);
+
 	printf("Day 2, Part 1: %d\n", memory_copy[0]);
 
 	// Part 2
