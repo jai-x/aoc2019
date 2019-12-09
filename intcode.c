3fee97c8dc625e7a5621ae423afa5676678b29fd
Jai Sharma <jai.p.sharma1@gmail.com>
2019-12-09 15:53:46 +0000

    Day 5 Part 2

--- intcode.c
+++ intcode.c
@@ -73,6 +73,38 @@ intcode(int* memory)
 				address += 2;
 				break;
 			}
+			// Jump to second value if first value is not zero
+			case 5: {
+				int val = first_val(memory, opcode, address);
+				int addr = second_val(memory, opcode, address);
+				address = (val != 0) ? addr : address + 3;
+				break;
+			}
+			// Jump to second value if first value is zero
+			case 6: {
+				int val = first_val(memory, opcode, address);
+				int addr = second_val(memory, opcode, address);
+				address = (val == 0) ? addr : address + 3;
+				break;
+			}
+			// If first value is less than the second value store 1 in third position, else store 0
+			case 7: {
+				int store = memory[address + 3];
+				int first = first_val(memory, opcode, address);
+				int second = second_val(memory, opcode, address);
+				memory[store] = (first < second) ? 1 : 0;
+				address += 4;
+				break;
+			}
+			// If first value is equal to the second value store 1 in third position, else store 0
+			case 8: {
+				int store = memory[address + 3];
+				int first = first_val(memory, opcode, address);
+				int second = second_val(memory, opcode, address);
+				memory[store] = (first == second) ? 1 : 0;
+				address += 4;
+				break;
+			}
 			case 99: {
 				return;
 			}
