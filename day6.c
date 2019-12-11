82ebe4157b583eb3ae79a61678973f49d065cdb7
Jai Sharma <jai.p.sharma1@gmail.com>
2019-12-11 23:33:14 +0000

    Day 6 Part 2

--- day6.c
+++ day6.c
@@ -10,6 +10,7 @@
 
 static const char* filename = "./input/day6.txt";
 
+// Orbit pair struct
 struct orbit_pair {
 	char* src;
 	char* dst;
@@ -24,11 +25,13 @@ make_orbit_pair(char* src, char* dst)
 	return p;
 }
 
+// Orbit pair list
 struct orbit_pair_list {
 	size_t num_pairs;
 	struct orbit_pair** pairs;
 };
 
+// Orbit pair list functions
 static struct orbit_pair_list*
 make_orbit_pair_list(FILE* file)
 {
@@ -82,15 +85,26 @@ print_orbit_pair_list(struct orbit_pair_list* l)
 	}
 }
 
+// Planet tree node
 struct planet {
 	char* name;
 	size_t orbits;
 	size_t num_children;
 	struct planet** children;
+	struct planet* parent;
 };
 
+/*
+static void
+print_planet(struct planet* p)
+{
+	printf("%s -> o: %zu, c: %zu\n", p->name, p->orbits, p->num_children);
+}
+*/
+
+// Planet tree functions
 static struct planet*
-make_planet_node(char* name, size_t orbits)
+make_planet_node(char* name, size_t orbits, struct planet* parent)
 {
 	struct planet* p = malloc(sizeof(struct planet));
 
@@ -98,30 +112,31 @@ make_planet_node(char* name, size_t orbits)
 	p->orbits = orbits;
 	p->num_children = 0;
 	p->children = NULL;
+	p->parent = parent;
 
 	return p;
 }
 
 static void
-make_planet_children(struct planet* node, struct orbit_pair_list* list)
+make_planet_children(struct orbit_pair_list* list, struct planet* node)
 {
 	for (size_t i = 0; i < list->num_pairs; i++) {
 		struct orbit_pair* pair = list->pairs[i];
 
 		if (strcmp(pair->src, node->name) == 0) {
 			node->children = realloc(node->children, sizeof(struct planet*) + (node->num_children + 1));
-			struct planet* child = make_planet_node(pair->dst, node->orbits + 1);
+			struct planet* child = make_planet_node(pair->dst, node->orbits + 1, node);
 			node->children[node->num_children++] = child;
 		}
 	}
 
 	for (size_t i = 0; i < node->num_children; i++) {
-		make_planet_children(node->children[i], list);
+		make_planet_children(list, node->children[i]);
 	}
 }
 
 static struct planet*
-make_planet_tree(char* start, struct orbit_pair_list* list)
+make_planet_tree(struct orbit_pair_list* list, char* start)
 {
 	bool exists = false;
 
@@ -139,8 +154,8 @@ make_planet_tree(char* start, struct orbit_pair_list* list)
 		return NULL;
 	}
 
-	struct planet* root = make_planet_node(start, 0);
-	make_planet_children(root, list);
+	struct planet* root = make_planet_node(start, 0, NULL);
+	make_planet_children(list, root);
 	return root;
 }
 
@@ -160,24 +175,69 @@ free_planet_tree(struct planet* node)
 static void
 print_planet_tree(struct planet* node, int indent)
 {
-	printf("%*s, o: %zu, c: %zu\n", indent, node->name, node->orbits, node->num_children);
+	printf("%*s", indent, "");
+	print_planet(node);
 
 	for (size_t i = 0; i < node->num_children; i++) {
-		print_planet_tree(node->children[i], indent + 5);
+		print_planet_tree(node->children[i], indent + 2);
 	}
 }
 */
 
+// Task functions
 static size_t
-planet_tree_total_orbits(struct planet* node)
+total_orbits(struct planet* node)
 {
 	size_t children_total = 0;
 
 	for (size_t i = 0; i < node->num_children; i++) {
-		children_total += planet_tree_total_orbits(node->children[i]);
+		children_total += total_orbits(node->children[i]);
 	}
 
 	return children_total + node->orbits;
+}
+
+static struct planet*
+find_planet(struct planet* node, char* to_find)
+{
+	if (strcmp(node->name, to_find) == 0) {
+		return node;
+	}
+
+	for (size_t i = 0; i < node->num_children; i++) {
+		struct planet* result = find_planet(node->children[i], to_find);
+		if (result) {
+			return result;
+		}
+	}
+
+	return NULL;
+}
+
+static struct planet*
+find_common_parent(struct planet* a, struct planet* b)
+{
+	if (a == b) {
+		return a;
+	}
+
+	struct planet* a_parent = a->parent;
+	struct planet* b_parent = b->parent;
+
+	while (a_parent) {
+		while (b_parent) {
+			if (a_parent == b_parent) {
+				return a_parent;
+			}
+
+			b_parent = b_parent->parent;
+		}
+
+		a_parent = a_parent->parent;
+		b_parent = b->parent;
+	}
+
+	return NULL;
 }
 
 void
@@ -187,16 +247,26 @@ day6(void)
 	assert(file);
 
 	struct orbit_pair_list* list = make_orbit_pair_list(file);
-	struct planet* com = make_planet_tree("COM", list);
+	struct planet* com_tree = make_planet_tree(list, "COM");
 
 	fclose(file);
 
-	// print_orbit_pair_list(list);
-	// print_planet_tree(com, 2);
+	printf("Day 6, Part 1: %zu\n", total_orbits(com_tree));
 
-	size_t total_orbits = planet_tree_total_orbits(com);
-	printf("Day 6, Part 1: %zu\n", total_orbits);
+	struct planet* you = find_planet(com_tree, "YOU");
+	struct planet* san = find_planet(com_tree, "SAN");
 
-	free_planet_tree(com);
+	struct planet* ys_parent = find_common_parent(you, san);
+
+	struct planet* ys_tree = make_planet_tree(list, ys_parent->name);
+
+	struct planet* ys_you = find_planet(ys_tree, "YOU");
+	struct planet* ys_san = find_planet(ys_tree, "SAN");
+
+	// Subtract 2, to exclude the counted orbit of YOU and SAN themselves
+	printf("Day 6, Part 2: %zu\n", ys_you->orbits + ys_san-> orbits - 2);
+
+	free_planet_tree(ys_tree);
+	free_planet_tree(com_tree);
 	free_orbit_pair_list(list);
 }
